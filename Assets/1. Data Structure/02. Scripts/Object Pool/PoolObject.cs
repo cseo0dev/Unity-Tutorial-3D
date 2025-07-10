b34061c08fb3e2d71c9972fdce2f21524dbe81a4
young <seoyoung1025s@gmail.com>
2025-07-10 18:23:02 +0900

    [ADD] Object Pooling
    2025. 07. 10.
    -- Queue 활용한 실습
    -- Object Pooling 구현

---
+++ Assets/1. Data Structure/02. Scripts/Object Pool/PoolObject.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PoolObject : MonoBehaviour
+{
+    private ObjectPoolQueue pool;
+    public float bulletSpeed = 50f;
+
+    void Awake()
+    {
+        pool = FindFirstObjectByType<ObjectPoolQueue>();
+    }
+
+    void OnEnable()
+    {
+        Invoke("ReturnPool", 3f);
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.forward * Time.deltaTime * bulletSpeed;
+    }
+
+    private void ReturnPool()
+    {
+        pool.EnqueueObject(gameObject);
+    }
+}
