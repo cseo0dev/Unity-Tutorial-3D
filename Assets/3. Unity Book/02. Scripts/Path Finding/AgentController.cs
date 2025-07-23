57916f9c9969345b06adaee2c46a79e3614275ee
young <seoyoung1025s@gmail.com>
2025-07-23 16:42:59 +0900

    [ADD] A* Algorithm, 3D FPS Game
    2025. 07. 23.
    -- 길 찾기 알고리즘 구현
    -- A* 알고리즘 구현
    -- 유니티를 통해 A* 알고리즘 구현

---
+++ Assets/3. Unity Book/02. Scripts/Path Finding/AgentController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    private Transform player;
+    private NavMeshAgent agent;
+
+    public Transform[] points;
+    public int index;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        SetRandomPoint();
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance <= 1.5f)
+        {
+            Debug.Log("목적지 변경");
+            SetRandomPoint();
+        }
+    }
+
+    private void SetRandomPoint()
+    {
+        int temp = index;
+
+        while (temp == index)
+        {
+            index = Random.Range(0, points.Length);
+        }
+
+        agent.SetDestination(points[index].position);
+    }
+}
